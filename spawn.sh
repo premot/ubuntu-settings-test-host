49525a3697b845c83a03db3582c207a3fabd6e69
n@x <n@x>
2026-07-21 00:20:49 +0300

    quick auto commit

--- spawn.sh
+++ spawn.sh
@@ -39,11 +39,12 @@ trap 'kill "$server"' EXIT INT TERM
 
 sudo virt-install \
   --name ubuntu-settings-test \
-  --memory 4096 \
+  --memory 8192 \
   --vcpus 2 \
   --disk size=32,bus=virtio \
   --os-variant ubuntu24.04 \
   --location "$ISO",kernel=casper/vmlinuz,initrd=casper/initrd \
   --extra-args 'autoinstall ds=nocloud-net;s=http://192.168.122.1:3003/' \
   --graphics spice
-#  --noautoconsole
+  # --noautoconsole
+  # --memory 4096 \
